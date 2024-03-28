38e495e5bcbf9ef410e708a2005a5a4f7930762c
laurenCho9 <lauren.choux@gmail.com>
2024-03-29 03:16:43 +0900

    feat: Redux Container를 Hook을 사용하도록 변경하기

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addTodo as addTodoActionCreator,
+  removeTodo as removeTodoActionCreator,
+  removeAll as removeAllActionCreator,
+} from "../redux/slices/todoSlice";
+import { fetchTodosRequested as fetchTodosRequestedActionCreator } from "../redux/actions/fetchTodosAction";
 
 function TodoApp(props) {
-  const {
-    // Redux state
-    todoItems,
-    //Redux dispatch
-    addTodo,
-    removeTodo,
-    removeAll,
-    triggerAsyncFunction,
-    fetchTodo,
-  } = props;
+  // const {
+  //   // Redux state
+  //   todoItems,
+  //   //Redux dispatch
+  //   addTodo,
+  //   removeTodo,
+  //   removeAll,
+  //   triggerAsyncFunction,
+  //   fetchTodo,
+  // } = props;
+
+  const todoItems = useSelector((state) => [
+    ...state.todo,
+    ...state.fetchTodos.data,
+  ]);
+  const dispatch = useDispatch();
 
   const [newTodo, setNewTodo] = useState("");
 
@@ -33,19 +46,32 @@ function TodoApp(props) {
         />
         <button
           onClick={() => {
-            addTodo(newTodo);
+            // addTodo(newTodo);
+            dispatch(addTodoActionCreator(newTodo));
             setNewTodo("");
           }}
         >
           할 일 추가
         </button>
-        <button onClick={removeTodo}>할 일 삭제</button>
-        <button onClick={removeAll}>모두 삭제</button>
+        <button
+          onClick={() => {
+            dispatch(removeTodoActionCreator());
+          }}
+        >
+          할 일 삭제
+        </button>
+        <button
+          onClick={() => {
+            dispatch(removeAllActionCreator());
+          }}
+        >
+          모두 삭제
+        </button>
       </div>
 
       <button
         onClick={() => {
-          triggerAsyncFunction((dispatch, getState) => {
+          dispatch((dispatch, getState) => {
             console.log(`비동기 함수 실행`, getState());
 
             new Promise((resolve, reject) => {
@@ -63,7 +89,13 @@ function TodoApp(props) {
         비동기 함수 테스트
       </button>
 
-      <button onClick={fetchTodo}>서버에서 할 일 목록 받아오기</button>
+      <button
+        onClick={() => {
+          dispatch(fetchTodosRequestedActionCreator());
+        }}
+      >
+        서버에서 할 일 목록 받아오기
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./redux/store.js";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import TodoAppContainer from "./redux/containers/TodoAppContainer.js";
+import TodoApp from "./components/TodoApp.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <TodoAppContainer />
+        <TodoApp />
       </PersistGate>
     </Provider>
   </React.StrictMode>
